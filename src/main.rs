9187e4f75a7a62e1e0d8cbb953ca562647b3f74b
Michal Widera <Michal.Widera@wdc.com>
2019-09-06 11:54:24 +0200

    dodanie test na var

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,65 @@ use num::complex::Complex;
 use num_rational::Ratio;
 use num_rational::Rational;
 
-fn sum(
-    vector_a: &[i32],
-    delta_a: Rational,
-    vector_b: &[i32],
-    delta_b: Rational,
-) -> Vec<(i32, i32)> {
-    let mut result = Vec::<(i32, i32)>::new();
+// summary of two time series and const delta
+fn sum(vec_a: &[isize], del_a: Rational, vec_b: &[isize], del_b: Rational) -> Vec<(isize, isize)> {
+    let mut result = Vec::<(isize, isize)>::new();
 
-    let delta_c = std::cmp::min(delta_a, delta_b);
+    let z = std::cmp::min(del_a, del_b);
 
     for i in 0..10 {
-        if delta_c == delta_a {
-            let idx = (Ratio::from_integer(i) * delta_a / delta_b).floor();
-            result.push((vector_a[i as usize], vector_b[*idx.numer() as usize]));
+        if z == del_a {
+            let idx = (Ratio::from_integer(i) * del_a / del_b).floor();
+            result.push((vec_a[i as usize], vec_b[*idx.numer() as usize]));
         } else {
-            let idx = (Ratio::from_integer(i) * delta_b / delta_a).floor();
-            result.push((vector_a[*idx.numer() as usize], vector_b[i as usize]));
+            let idx = (Ratio::from_integer(i) * del_b / del_a).floor();
+            result.push((vec_a[*idx.numer() as usize], vec_b[i as usize]));
         }
     }
 
     result
 }
 
-fn hash(vector_a: &[i32], delta_a: Rational, vector_b: &[i32], delta_b: Rational) -> Vec<i32> {
-    let mut result = Vec::<i32>::new();
+// hash function on two time series and const delta
+fn hash(vec_a: &[isize], del_a: Rational, vec_b: &[isize], del_b: Rational) -> Vec<isize> {
+    let mut result = Vec::<isize>::new();
 
-    let delta = delta_b / (delta_a + delta_b);
+    let z = del_b / (del_a + del_b);
 
     for i in 0..20 {
         let idx = Ratio::from_integer(i);
-        if (delta * idx).floor() == ((idx + 1) * delta).floor() {
-            result.push(vector_b[*(idx - ((idx + 1) * delta).floor()).numer() as usize]);
+        if (z * idx).floor() == ((idx + 1) * z).floor() {
+            result.push(vec_b[*(idx - ((idx + 1) * z).floor()).numer() as usize]);
         } else {
-            result.push(vector_a[*((idx * delta).floor()).numer() as usize]);
+            result.push(vec_a[*((idx * z).floor()).numer() as usize]);
+        }
+    }
+
+    result
+}
+
+// testing on accelerated delta
+fn hash_test(vec_a: &[isize], vec_b: &[isize]) -> Vec<isize> {
+    fn del_a(n: isize) -> Rational {
+        Ratio::new(1, 2) + Ratio::new(n, 20)
+    }
+
+    fn del_b(n: isize) -> Rational {
+        Ratio::new(1, 1) + Ratio::new(n, 10)
+    }
+
+    fn z(n: isize) -> Rational {
+        del_b(n) / (del_a(n) + del_b(n))
+    }
+
+    let mut result = Vec::<isize>::new();
+
+    for i in 0..20 {
+        let idx = Ratio::from_integer(i);
+        if (z(i) * idx).floor() == ((idx + 1) * z(i + 1)).floor() {
+            result.push(vec_b[*(idx - ((idx + 1) * z(i)).floor()).numer() as usize]);
+        } else {
+            result.push(vec_a[*((idx * z(i)).floor()).numer() as usize]);
         }
     }
 
@@ -43,8 +68,8 @@ fn hash(vector_a: &[i32], delta_a: Rational, vector_b: &[i32], delta_b: Rational
 }
 
 fn main() {
-    let vector_a: Vec<i32> = (0..30).map(|x| x).collect();
-    let vector_b: Vec<i32> = (0..30).map(|x| -x).collect();
+    let vector_a: Vec<isize> = (1..30).map(|x| x).collect();
+    let vector_b: Vec<isize> = (1..30).map(|x| -x).collect();
 
     let delta_a = Ratio::new(1, 2);
     let delta_b = Ratio::new(1, 1);
@@ -66,4 +91,7 @@ fn main() {
 
     let v_result = hash(&vector_a, delta_a, &vector_b, delta_b);
     println!("hash: {:?}", v_result);
+
+    let v_result = hash_test(&vector_a, &vector_b);
+    println!("hash_test: {:?}", v_result);
 }
